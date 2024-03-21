a742066ae823a4fc17f547cdb89a93bbecd6fa39
eduuardo1357 <eduardo.silva1077@etec.sp.gov.br>
2024-03-21 08:38:46 -0300

    DDM_Prova

---
+++ Prova_DDM_1/SRC/componets/Eduardo/main.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Eduardo() {
+  return (
+    <View style={styles.container}>
+      <Text>Eduardo Dias Silva</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
---
+++ Prova_DDM_1/SRC/componets/Gabriel/index.js
@@ -0,0 +1,12 @@
+import react from "react";
+import { View, Text } from 'react-native';
+
+export default function Gabriel(){
+    return(
+           <View>
+            <Text>Gabriel Comunhão Gardini</Text>
+           </View>
+         
+    );
+}
+
